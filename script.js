aba5b11979b96b7e9df9e26527564769af4d9a41
Ganesh Paware <166027579+GANESHpaware-05@users.noreply.github.com>
2024-04-08 21:00:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction1() {
+    // Get the text field
+    var copyText = document.getElementById("blake2s_output");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+
+    document.getElementById("copy1").innerText="Value Copied !!"; 
+}
